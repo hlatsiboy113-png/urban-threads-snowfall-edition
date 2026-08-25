635e4520f9c49f549eacb514156abb91bedf6d72
Urban Threads Dev <dev@urbanthreads.local>
2026-08-25 11:53:46 +0200

    feat: connect Urban Threads to Firebase backend

--- js/firebase.js
+++ js/firebase.js
@@ -3,15 +3,17 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDNx802wA6ony2ACYyZRUsHkW0ZdziuP4A",
+  authDomain: "urban-threadz-snowfall-edition.firebaseapp.com",
+  projectId: "urban-threadz-snowfall-edition",
+  storageBucket: "urban-threadz-snowfall-edition.firebasestorage.app",
+  messagingSenderId: "514741001875",
+  appId: "1:514741001875:web:42650281ac873d326f3766"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 export default app;
